f722cfcd70d70dd62afde83a47c88145ffb0b716
sunnydodti <sunnydodti@gmail.com>
2025-04-03 13:58:22 +0530

    cors

--- url-api/src/index.ts
+++ url-api/src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
 import { routes } from "./routes";
 import { handleDbConnection } from "./db";
+import { cors } from 'hono/cors'
 
 const app = new Hono<{ Bindings: Env }>();
 
 // Middleware to handle database connection
 app.use("*", handleDbConnection);
-
+app.use(cors());
 // Mount the routes
 app.route("/", routes);
 
